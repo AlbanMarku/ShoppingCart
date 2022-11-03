8e88ef5cae2036422700688999769aabe5649fc5
AlbanMarku <albanmarku@outlook.com>
2022-11-03 19:38:03 +0000

    Prettier setup.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,22 +6,18 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb/hooks',
-    "airbnb-typescript",
+    'airbnb-typescript',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  overrides: [
-  ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: "./tsconfig.json"
+    project: './tsconfig.json',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
 };
