47b3827c1705a6c9bd71527a451a723c114ecf1e
AlbanMarku <albanmarku@outlook.com>
2022-11-04 17:46:10 +0000

    Basic item buttons.

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -8,12 +8,28 @@ interface ItemCardProps {
 }
 
 function ItemCard({ id, name, price, img }: ItemCardProps) {
+  const quantity = 0;
   return (
     <div className="ItemCard">
-      <h2>{name}</h2>
-      <h3>{formatCurrency(price)}</h3>
-      <img src={img} alt="product" />
-      <p>{id}</p>
+      <div className="infoArea">
+        <h2>{name}</h2>
+        <h3>{formatCurrency(price)}</h3>
+        <img src={img} alt="product" />
+        <p>{id}</p>
+      </div>
+      {/* Sort styling for separate conditions */}
+      <div className="buttonArea">
+        {quantity === 0 ? (
+          <button type="button">Add to cart</button>
+        ) : (
+          <>
+            <button type="button">Less</button>
+            <p>Amount goes here</p>
+            <button type="button">More</button>
+            <button type="button">Remove</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
